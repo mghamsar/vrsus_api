085e40adafa82250542675180a22dea5e60f3529
Mahtab <mahtab.ghamsari@gmail.com>
2016-07-05 17:13:04 +0100

    test3

--- application.py
+++ application.py
@@ -31,8 +31,9 @@ def download():
 	print videoname
 	s3 = boto.connect_s3(aws_access_key_id = s3_access_key, aws_secret_access_key = s3_secret_key)
 	v = s3.get_bucket('vrsuscovideos').get_key(videoname)
+
 	#v.get_contents_to_filename(url+videoname)
-	return "done"
+	return "done"+url+videoname
 
 
 #@app.route("/video/<videoname>", methods=['GET'])
