bf147dfe319eed975b727423c41f5a3c8057898d
Mahtab <mahtab.ghamsari@gmail.com>
2016-07-07 13:41:55 +0100

    add mysql from rds

--- application.py
+++ application.py
@@ -1,24 +1,35 @@
 from flask import Flask, request, render_template, send_file, send_from_directory, make_response
-from flask import Response
+from flask import Response, jsonify
+from flask.json import JSONEncoder
 from flask.ext.mysql import MySQL
+from datetime import datetime
 import boto
+import json
 import boto.s3.connection
 import os
 import tempfile
+from decimal import Decimal
 
 application = app = Flask(__name__)
-#mysql = MySQL()
+mysql = MySQL()
 
-#app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
-#app.config['MYSQL_DATABASE_DB'] = 'vrsus'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-#mysql.init_app(app)
+app.config['MYSQL_DATABASE_USER'] = 'mghamsar'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'AminMandana%1362'
+app.config['MYSQL_DATABASE_DB'] = 'vrsus'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_HOST'] = 'vrsus.c7e2fotesw6y.us-east-1.rds.amazonaws.com'
+
+mysql.init_app(app)
 
 s3_access_key = 'AKIAIJP6FY37QAB4C5VA'
 s3_secret_key = 's85klsigZGf5cUr+IcoaZhwNULSSMN/uqRYltDbG'
 
-#"AKIAIJP6FY37QAB4C5VA","s85klsigZGf5cUr+IcoaZhwNULSSMN/uqRYltDbG"
+class fakefloat(float):
+    def __init__(self, value):
+        self._value = value
+    def __repr__(self):
+        return str(self._value)
+
 @app.route("/")
 def index():
 	return render_template('basic.html')
@@ -26,31 +37,8 @@ def index():
 
 @app.route('/download', methods=['POST'])
 def download():
-	url = request.form['download_path']
 	videoname = request.form['video_name']
-	s3 = boto.connect_s3(aws_access_key_id = s3_access_key, aws_secret_access_key = s3_secret_key)
-	v = s3.get_bucket('vrsuscovideos').get_key(videoname)
-	v.get_contents_to_filename(videoname)
-
-	if v: 
-		tempfilename = tempfile.mktemp(prefix='tmp_%s_%s' % ('o', videoname), dir=os.path.abspath(os.path.dirname(__file__)))
-		filetype = v.get_file(open(tempfilename, "w"))
-		cachefilename = "cache_"+videoname
-		os.rename(tempfilename, cachefilename)
-	
-		LOCAL_PATH = os.path.abspath(os.path.dirname(__file__))
-		print "Server Path: "+ LOCAL_PATH + "----video name: " + videoname
-		
-		with open(videoname, 'rb') as f:
-			body = f.read()
-			response = make_response(body)
-			response.headers['Content-Description'] = 'File Transfer'
-			response.headers['Cache-Control'] = 'no-cache'
-			response.headers['Content-Type'] = 'application/octet-stream'
-			response.headers['Content-Disposition'] = 'attachment; filename=%s' % videoname
-			#response.headers['X-Accel-Redirect'] = server_path
-
-		return response
+	return get_video(videoname)
 
 
 @app.route("/video/<videoname>", methods=['GET'])
@@ -71,20 +59,49 @@ def get_video(videoname):
 
 		return response
 
-#@app.route("/Authenticate")
-#def Authenticate():
+@app.route("/venues/<venuename>")
+def get_venue(venuename):
 	#username = request.args.get('UserName')
 	#password = request.args.get('Password')
-	#cursor = mysql.connect().cursor()
-	#cursor.execute("SELECT * from users where Username='" + username + "' and Password='" + password + "'")
-	#data = cursor.fetchone()
-	#return "yo"
-#	if data is None:
-#		return "Username or Password is wrong"
-#	else:
-#		return "Logged in successfully"
+
+	def date_handler(obj):
+	    if hasattr(obj, 'isoformat'):
+	        return obj.isoformat()
+	    else:
+	        raise TypeError
 
 
+	def defaultencode(o):
+	    if isinstance(o, Decimal):
+	        # Subclass float with custom repr?
+	        return fakefloat(o)
+	    raise TypeError(repr(o) + " is not JSON serializable")
+
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from venues where name='" + venuename + "'")
+	data = cursor.fetchall()
+
+	results = {}
+	for row, values in enumerate(data):
+		print row, values
+
+		results = {'id':values[0], 'venuename':values[1],
+		   'lat':values[3],
+		   'long':values[4],
+		   'date_added':date_handler(values[5]),
+		   'date_updated':date_handler(values[6])
+		}
+
+	r = results
+	print r
+
+	if results is None:
+		return "Venue name did not return any results"
+	else:
+		return json.dumps(results,default=defaultencode)
+		#jsonify(data=r)
+
+#################---------------------###################
 
 if __name__=="__main__":
 	application.debug = True
