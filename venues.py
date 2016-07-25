35a355410a4b227698fa807470dbd3dbbb77d0e1
Mahtab <mahtab.ghamsari@gmail.com>
2016-07-25 16:11:18 +0100

    add count to venues

--- venues.py
+++ venues.py
@@ -2,6 +2,7 @@ import MySQLdb
 import boto, boto.s3.connection
 from flask import jsonify, request, Response
 from config import Config
+from db import Db
 
 
 class Venues:
@@ -22,65 +23,40 @@ class Venues:
 
     def getVenues(self):
         
-        dbConnect = MySQLdb.connect(host=Config.MYSQL_DATABASE_HOST, user=Config.MYSQL_DATABASE_USER, 
-            passwd=Config.MYSQL_DATABASE_PASSWORD,
-            db=Config.MYSQL_DATABASE_DB,
-            port=Config.MYSQL_DATABASE_PORT)
-        
-        cursor =dbConnect.cursor()
-
-        query = "SELECT * from venues"
-
         event = request.args.get('event') if request.args.get('event') is not None else None
         category = request.args.get('category') if request.args.get('category') is not None else None
+        #name = request.args.get('name') if request.args.get('name') is not None else None
+        count = request.args.get('count') if request.args.get('count') is not None else None
         event_id = None
 
+        query = "SELECT * from venues";
+
+        #if name is not None:
+        #    query = query + " as v where v.name='"+name+"'";
+
         if event is not None:
-            query = query + " as v JOIN events as e where v.id=e.venue_id AND e.event_name='"+event+"'"
+            query = query + " as v JOIN events as e where v.id=e.venue_id AND e.event_name='"+event+"'";
 
         if category is not None:
-            query = query + " as v JOIN events as e where v.id=e.venue_id AND e.category='"+category+"'"
+            query = query + " as v JOIN events as e where v.id=e.venue_id AND e.category='"+category+"'";
+
+        if count is not None:
+             query = query + " LIMIT "+str(count);
 
         query = query +";"
 
-        try:
-            cursor.execute(query)
-            data = cursor.fetchall()
-        except MySQLdb.Error, e:
-            try:
-                print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                return None
-            except IndexError:
-                print "MySQL Error: %s" % str(e)
-                return None
+        dbi = Db();
+        data = dbi.runQuery(query);
 
         if category is not None and len(data) == 0:
             # Try a second query with lower case letters 
             query = query + " as v JOIN events as e where v.id=e.venue_id AND e.event_name='"+category.lower()+"'"
-            try:
-                cursor.execute(query)
-                data = cursor.fetchall()
-            except MySQLdb.Error, e:
-                try:
-                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                    return None
-                except IndexError:
-                    print "MySQL Error: %s" % str(e)
-                    return None
+            data = dbi.runQuery(query);
 
         elif event is not None and len(data) == 0:
             # Try a second query with lower case letters 
             query = query + " as v JOIN events as e where v.id=e.venue_id AND e.event_name='"+event.lower()+"'"
-            try:
-                cursor.execute(query)
-                data = cursor.fetchall()
-            except MySQLdb.Error, e:
-                try:
-                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                    return None
-                except IndexError:
-                    print "MySQL Error: %s" % str(e)
-                    return None
+            data = dbi.runQuery(query);
 
         results = {}
         if len(data) >= 1:
@@ -113,25 +89,12 @@ class Venues:
 
         # Get values from query string
         event = request.args.get('event') if request.args.get('event') is not None else None
+        count = request.args.get('count') if request.args.get('count') is not None else None
 
         results = {}
         try:
             cursor.execute("SELECT * from venues where name='" + venuename + "'")
             data = cursor.fetchall()
-
-            for row, values in enumerate(data):
-                results = {'id':values[0],
-                   'venuename':values[1],
-                   'lat':str(values[2]),
-                   'long':str(values[3]),
-                   'date_added':self.date_handler(values[4]),
-                   'date_updated':self.date_handler(values[5])
-                }
-            if results is None:
-                return "Venue name did not return any results"
-            else:
-                return jsonify(data=results)
-
         except MySQLdb.Error, e:
             try:
                 print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
@@ -148,6 +111,18 @@ class Venues:
         finally:
             cursor.close()
             dbConnect.close()
+
+        if len(data)>=1:
+            for row, values in enumerate(data):
+                results = {'id':values[0],
+                   'venuename':values[1],
+                   'lat':str(values[2]),
+                   'long':str(values[3]),
+                   'date_added':self.date_handler(values[4]),
+                   'date_updated':self.date_handler(values[5])
+                }
+
+        return jsonify(data=results)
 
 # class fakefloat(float):
 #     def __init__(self, value):
