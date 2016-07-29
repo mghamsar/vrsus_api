5bce3d9265897c9294a3650a0ac9ab2d18ad369a
Mahtab <mahtab.ghamsari@gmail.com>
2016-07-29 15:28:27 +0100

    update images call to DB

--- images.py
+++ images.py
@@ -3,6 +3,7 @@ import boto, boto.s3.connection
 from flask import jsonify, request, Response, make_response
 from config import Config
 import json
+from db import Db
 
 class Images:
 
@@ -44,23 +45,9 @@ class Images:
 
         query = query +";"
 
-        dbConnect = MySQLdb.connect(host=Config.MYSQL_DATABASE_HOST, user=Config.MYSQL_DATABASE_USER, 
-            passwd=Config.MYSQL_DATABASE_PASSWORD,
-            db=Config.MYSQL_DATABASE_DB,
-            port=Config.MYSQL_DATABASE_PORT)
-        
-        cursor = dbConnect.cursor()
-        try:
-            cursor.execute(query)
-            data = cursor.fetchall()
-        except MySQLdb.Error, e:
-            try:
-                print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                return None
-            except IndexError:
-                print "MySQL Error: %s" % str(e)
-                return None
-            
+        dbi = Db();
+        data = dbi.getQuery(query);
+    
         results = {}
         responses = []
         if len(data) >= 1:
@@ -79,9 +66,4 @@ class Images:
 
                 responses.append(results[row]['file_name'])
 
-            cursor.close()
-            dbConnect.close()
-            return jsonify(data=responses)
-        else:
-            dbConnect.close()
-            return "No Image Ids Found with the Specified Search"
+        return jsonify(data=responses)
