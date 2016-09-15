95f4820908163324c0b1a85425511f94d33fca68
mghamsar <mahtab.ghamsari@gmail.com>
2016-09-15 19:24:08 +0100

    change app to application

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from config import Config
 import os
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mghamsar:vrsus2016@vrsus.c7e2fotesw6y.us-east-1.rds.amazonaws.com/vrsus_dev'
 db = SQLAlchemy(app)
--- run.py
+++ run.py
@@ -1,2 +1,3 @@
-from app import app
-app.run(debug=True)
+from app import application
+
+application.run(debug=True)
